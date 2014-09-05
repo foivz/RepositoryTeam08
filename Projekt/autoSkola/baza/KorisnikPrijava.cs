d9725a5562c8b391a6aa076b267057e84df5517e
gabrijela28 <ggrgaca@foi.hr>
2014-09-05 12:55:57 +0200

    Dodana nova klasa
    Preseljen kod za autorizaciju korisnika iz sloja sučelja (frmPrijava) u
    novu klasu KorisnikPrijava

---
+++ Projekt/autoSkola/baza/KorisnikPrijava.cs
@@ -0,0 +1,85 @@
+﻿using autoSkola.forme;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace autoSkola
+{
+    public static class KorisnikPrijava
+    {
+        public static bool verificirajKorisnika(string korisnickoIme, string lozinka, ref data podaci)
+        {
+            korisnik Korisnik = new korisnik();
+            bool imaNeta = true;
+            string sqlUpit = "SELECT ID_korisnik FROM korisnik WHERE korisnicko_ime='" + korisnickoIme + "' and lozinka='" + lozinka + "';";
+            int ID = 0;
+            try
+            {
+                ID = (int)baza.Instanca.DohvatiVrijednost(sqlUpit);
+            }
+            catch (Exception)
+            {
+                imaNeta = false;
+            }
+            string korisnikTip = null;
+            if (imaNeta)
+                korisnikTip = Korisnik.Provjera(korisnickoIme, lozinka);
+            else
+            {
+                string docPath = "xml/korisnik.xml";
+                if (File.Exists(docPath))
+                {
+                    XDocument xml = XDocument.Load(docPath);
+                    foreach (var el in xml.Descendants("korisnik"))
+                    {
+                        string korIme = el.Element("korisnicko_ime").Value.ToString().Trim();
+                        string pass = el.Element("lozinka").Value.ToString().Trim();
+                        if (korIme == korisnickoIme && pass == lozinka)
+                        {
+                            korisnikTip = el.Element("ID_korisnikTip").Value.ToString();
+                            break;
+                        }
+                    }
+                    MessageBox.Show("Niste spojeni na bazu podataka!");
+                }
+                else
+                    MessageBox.Show("Provjerite svoju internet vezu!");
+            }
+
+            if (korisnikTip == null)
+            {
+                MessageBox.Show("Korisnik ne postoji!");
+            }
+            else
+            {
+
+                podaci = new data(korisnickoIme, lozinka, korisnikTip);
+
+                if (int.Parse(korisnikTip) == 100)
+                {
+                    forme.frmUcenik forma = new frmUcenik(ID.ToString());
+                    forma.ShowDialog();
+                }
+                else if (int.Parse(korisnikTip) == 101 || int.Parse(korisnikTip) == 102)
+                {
+                    formUciteljGlavni frmUciteljGlavni = new formUciteljGlavni(podaci, ID);
+                    frmUciteljGlavni.Show();
+                }
+                else
+                {
+                    forme.frmGlavna glavna = new forme.frmGlavna();
+                    glavna.ShowDialog();
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
--- Projekt/autoSkola/forme/formPrijava.cs
+++ Projekt/autoSkola/forme/formPrijava.cs
@@ -18,6 +18,7 @@ namespace autoSkola
     {////
 
         public data podaci;
+
         public formPrijava()
         {
             InitializeComponent();
@@ -25,99 +26,13 @@ namespace autoSkola
         private void btnPrijava_Click(object sender, EventArgs e)
         {
             
-            korisnik Korisnik = new korisnik(); 
-            bool imaNeta = true;
-            string sqlUpit = "SELECT ID_korisnik FROM korisnik WHERE korisnicko_ime='" + txtKorIme.Text + "' and lozinka='" + txtLozinka.Text + "';";
-            int ID = 0;
-            try
-            {
-                ID = (int)baza.Instanca.DohvatiVrijednost(sqlUpit);
-            }
-            catch (Exception)
-            {
-                imaNeta = false;
-            }
-            string korisnikTip = null;
-            if (imaNeta)
-                korisnikTip = Korisnik.Provjera(txtKorIme.Text, txtLozinka.Text);
-            else 
-            { 
-                string docPath = "xml/korisnik.xml";
-                if (File.Exists(docPath))
-                {
-                    XDocument xml = XDocument.Load(docPath);
-                    foreach (var el in xml.Descendants("korisnik"))
-                    {
-                        string korIme = el.Element("korisnicko_ime").Value.ToString().Trim();
-                        string lozinka = el.Element("lozinka").Value.ToString().Trim();
-                        if (korIme == txtKorIme.Text && lozinka == txtLozinka.Text)
-                        {
-                            korisnikTip = el.Element("ID_korisnikTip").Value.ToString();
-                            break;
-                        }
-                    }
-                    MessageBox.Show("Niste spojeni na bazu podataka!");
-                }
-                else
-                    MessageBox.Show("Provjerite svoju internet vezu!");
-            }
-                
-            if (korisnikTip == null)
-            {
-                MessageBox.Show("Korisnik ne postoji!");
-            }
-            else
-            {
-
-                podaci = new data(txtKorIme.Text, txtLozinka.Text, korisnikTip);
-
-                if (int.Parse(korisnikTip)==100)
-                {
-                    forme.frmUcenik forma = new frmUcenik(ID.ToString());
-                    forma.ShowDialog();
-                }
-                else if (int.Parse(korisnikTip) == 101 || int.Parse(korisnikTip) == 102)
-                {
-                    formUciteljGlavni frmUciteljGlavni = new formUciteljGlavni(podaci,ID);
-                    this.Hide();
-                    frmUciteljGlavni.Show();
-                }
-                else
-                {
-                    forme.frmGlavna glavna = new forme.frmGlavna();
-                    glavna.ShowDialog();
-                }
-                #region obrisano
-                ///<summary>
-                /// back je dretva koja periodično provjerava postoje li promjene u klasama i ažurira ih
-                ///</summary>
-                //System.Threading.Thread back;
-                //updateDB ba = new updateDB();
-                //ba.DT = podaci;
-                //back = new System.Threading.Thread(ba.startUpdateDB);
-                //back.IsBackground = true;
-                //back.Start();
-                
-                //System.Threading.Thread back2;
-                //updateFTP fp = new updateFTP(podaci);
-                #endregion
-            }
+           if (KorisnikPrijava.verificirajKorisnika(txtKorIme.Text, txtLozinka.Text, ref podaci))
+           {
+               this.Hide();
+           }
 
         }
-        #region obrisano 2
-        /// <summary>
-        /// override Close (x) btn!
-        /// </summary>
-        /// <param name="e"> form closing event</param>
-        //protected override void OnFormClosing(FormClosingEventArgs e)
-        //{
-        //    base.OnFormClosing(e);
-        //    if (e.CloseReason == CloseReason.WindowsShutDown) 
-        //        return;
-        //    else
-        //        baza.Instanca.Zatvori(podaci, false);
-        //}
-        #endregion
+
         private void formPrijava_Load(object sender, EventArgs e)
         {
 
